630d18baf56b8effab0171a9ae3a386393f2533b
Elizabeth "Elise" Alvarado <danceswithtime@gmail.com>
2019-01-11 11:19:43 -0600

    Begin UFO Tutorial
    https://unity3d.com/learn/tutorials/projects/2d-ufo-tutorial/controlling-player?playlist=25844

---
+++ UFO-Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        float move_Horizontal = Input.GetAxis("Horizontal");
+        float move_Vertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(move_Horizontal, move_Vertical);
+        rb2d.AddForce(movement);
+    }
+}

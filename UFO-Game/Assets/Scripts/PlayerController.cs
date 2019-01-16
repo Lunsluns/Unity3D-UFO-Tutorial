33f22272ed452da87f4f0ae10f0d5cb03cbe3526
Elizabeth "Elise" Alvarado <danceswithtime@gmail.com>
2019-01-16 10:11:58 -0600

    blep-- finished

--- UFO-Game/Assets/Scripts/PlayerController.cs
+++ UFO-Game/Assets/Scripts/PlayerController.cs
@@ -1,15 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
+    private int count;
     public float speed;
     private Rigidbody2D rb2d;
 
+    //Text, public
+    public Text count_Text;
+    public Text win_Text;
+
     private void Start()
     {
+        count = 0;
         rb2d = GetComponent<Rigidbody2D>();
+        win_Text.text = "";
+        SetCountText();
     }
 
     private void FixedUpdate()
@@ -18,5 +27,21 @@ public class PlayerController : MonoBehaviour
         float move_Vertical = Input.GetAxis("Vertical");
         Vector2 movement = new Vector2(move_Horizontal, move_Vertical);
         rb2d.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+    void SetCountText()
+    {
+        count_Text.text = "Count: " + count.ToString();
+        if (count >= 12)
+            win_Text.text = "You win!";
     }
 }
---
+++ UFO-Game/Assets/Scripts/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    { //Rotate transform of game object this is attached to by 15 degrees; takes account time elapsed since last frame.
+        transform.Rotate(new Vector3(0, 0, 15) * Time.deltaTime);
+    }
+}
